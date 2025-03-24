ffb10515f0854b520dfec17fbdc195414af0306a
JmZacapaDev <jmzacapa.dev@gmail.com>
2025-03-23 20:38:38 -0600

    feat: Implement BookInfoService to fetch book details from API

--- src/main.py
+++ src/main.py
@@ -1,29 +1,29 @@
 from barcode_validator import BarcodeValidator
 from barcode_processor import BarcodeProcessor
-from barcode_scanner import KeyboardBarcodeScanner
-from database_manager import DatabaseManager
+from barcode_scanner   import KeyboardBarcodeScanner
+from database_manager  import DatabaseManager
+from book_info_service import BookInfoService
 
-'''
-Scanner test
+def main():
+    book_service = BookInfoService()
+    storage = DatabaseManager()
+
+    # Sample ISBN
+    isbn = "9781234567890"
+
+    book_data = book_service.fetch_book_details(isbn)
+
+    if book_data:
+        print(f'Fetched Book: {book_data}')
+
+        storage.add_book(book_data["isbn"], book_data["title"], book_data["author"])
+
+        book = storage.get_book(book_data["isbn"])
+        print(f'Stored book:{book}')
+
+        success = storage.remove_book(book_data["isbn"])
+        print("Book removed" if success else "Book not found")
+
+
 if __name__ == "__main__":
-    validator = BarcodeValidator()
-    scanner = KeyboardBarcodeScanner()
-    processor = BarcodeProcessor(scanner, validator)
-
-    barcode = processor.insert_barcode()
-'''
-
-if __name__ == "__main__":
-    db = DatabaseManager()
-
-    # Add a book
-    db.add_book("9781234567890", "The Great Gatsby", "F. Scott Fitzgerald")
-
-    # Retrieve a book
-    book = db.get_book("9781234567890")
-    print(book)  # ✅ Should print book details
-
-    # Remove a book
-    success = db.remove_book("9781234567890")
-    print("Book removed" if success else "Book not found")
-
+    main()
