e40d90527d4aa37aac11d4b307970beec8d01d08
JmZacapaDev <jmzacapa.dev@gmail.com>
2025-04-24 23:21:45 -0600

    Json and CSV suport to extract save files.

--- src/book_manager.py
+++ src/book_manager.py
@@ -1,5 +1,11 @@
+import csv
+import json
 from database_manager import DatabaseManager
 from book_info_service import BookInfoService
+from logger import get_logger
+
+logger = get_logger(__name__)
+
 
 class BookManager:
     """Handle Book CRUD operations"""
@@ -22,6 +28,37 @@ class BookManager:
         """Removes a book by ISBN."""
         return self.storage.remove_book(isbn)
 
+    def export_to_csv(self, file_path: str) -> None:
+        """Save all books info in a CSV File"""
+        books = self.storage.get_all_books()
+
+        if not books:
+            logger.error(f"No books to export.")
+
+        with open(file_path, mode='w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["id", "isbn", "title", "author"])
+            writer.writeheader()
+            writer.writerows(books)
+            logger.info(f"Books info saved in CSV file at: {file_path}")
+
+
+    def export_to_json(self, file_path: str) -> None:
+        """Save all books info in a json File"""
+        books = self.storage.get_all_books()
+
+        if not books:
+            logger.error(f"No books to export.")
+            return
+
+
+        try:
+            with open(file_path, "w", encoding="utf-8") as jsonf:
+                json.dump(books,jsonf,ensure_ascii = False, indent=4)
+            logger.info(f"books info saved to Json file at {file_path}")
+
+        except Exception as e:
+            logger.error(f"Failed to export books to JSON {e}")
+
 if __name__ == "__main__":
     manager = BookManager()
 
@@ -31,7 +68,8 @@ if __name__ == "__main__":
         "author": "Sahan Wiratunga"
     }
 
-    manager.add_book(book_data)
+    manager.add_book(book_data["isbn"], book_data["title"], book_data["author"])
     print(manager.get_books())
     print(manager.find_book("9781234567890"))
-    print(manager.delete_book("9781234567890"))
+    manager.export_to_json("my_books.json")
+    print(manager.remove_book("9781234567890"))
